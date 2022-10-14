9fae8753f82e711ccb96623a75890f5a7cc7a07c
Matheus Rodrigues Cavalcante <96153654+rodriguesmatpc@users.noreply.github.com>
2022-10-14 10:25:56 -0300

    First commit

---
+++ teste.cs
@@ -0,0 +1,91 @@
+/*using System;
+
+namespace CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(5 / 10);
+
+
+            // ----------------------------------------*EXERCÍCIO DE ACRESCIMO E DECRESCIMO DE VALORES*-----------------------------------------------
+
+            int value = 5;
+
+            value = value + 5;
+            Console.WriteLine("Primeira incrementação: " + value);
+
+            value += 5;
+            Console.WriteLine("Segunda incrementação: " + value);
+
+            value++;
+            Console.WriteLine("Terceira incrementação: " + value);
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            value = value - 5;
+            Console.WriteLine("Primeiro decrescimento: " + value);
+
+            value -= 5;
+            Console.WriteLine("Segundo decrescimo: " + value);
+
+            value--;
+            Console.WriteLine("Terceiro decrescimo: " + value);
+
+
+            Console.WriteLine("------------*ACRESCENTANDO APENAS UM VALOR*-----------------------------");
+
+            int value = 1;
+            value++;
+            Console.WriteLine("Primeiro: " + value);
+            Console.WriteLine("Segundo: " + value++);
+            Console.WriteLine("Terceiro: " + value);
+            Console.WriteLine("Quarto: " + (++value));
+
+            Console.WriteLine("------------------DIMINUINDO(DECRESCENDO)UM VALOR QUALQUER\{(Ex: Diminuindo --um ")\n;
+            int value = 1;
+            value = 1;
+            value--;
+            Console.WriteLine("Primeiro: " + value);
+            Console.WriteLine("Segundo: " + value++);
+            Console.WriteLine("Terceiro: " + value);
+            Console.WriteLine("Quarto: " + (++value));
+
+
+            int value = 1;
+            value = 1;
+            value--;
+            Console.WriteLine("Primeiro: " + value);
+            Console.WriteLine("Segundo: " + value--);
+            Console.WriteLine("Terceiro: " + value);
+            Console.WriteLine("Quarto: " + (--value));
+
+
+            // -----------------CONVERTENDO FAHRENHEIT PARA CELCIUS------------------------------------------------------------------------
+            int fahrenheit = 94;
+            fahrenheit = fahrenheit - 32;
+            int celcius = 0;
+            celcius = fahrenheit;
+
+
+
+            Console.WriteLine("94 - 32 = " + celcius);
+            Console.WriteLine("The temperature is: 62 * (5/9) = " + celcius * 5 / 9m + " Celcius");
+
+            //       -----------MS Solution------------
+
+            int fahrenheit = 94;
+            decimal celcius = (fahrenheit - 32m) * (5m / 9m);
+
+            Console.WriteLine("The temperature is " + celcius + " Celcius");
+
+            // ----------------------------------------------------------------------------------
+
+
+
+
+        }
+    }
+}*/
